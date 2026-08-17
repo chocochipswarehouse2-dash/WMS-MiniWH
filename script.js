9818789b84a548592162ef0cb85d086b0370dd8c
chocochipswarehouse2-dash <chocochips.warehouse2@gmail.com>
2026-08-17 15:35:06 +0700

    chore: update GAS deployment URL to @32

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const GOOGLE_SHEET_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwzme5WXV7_sY87iCjz7RyVT8IV5q6T2stIGpqPbDph5UDBDCFH1VVoCRjgXZWB2e6k/exec";
+const GOOGLE_SHEET_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzbydk8A6yPv0FcwI4QhhBEM5pAmW_ivNEbW3Mkb6GZYXjxIeEnUxvePC5vSjCq5CSy/exec";
 
 const state = { 
   currentUser: null, 
